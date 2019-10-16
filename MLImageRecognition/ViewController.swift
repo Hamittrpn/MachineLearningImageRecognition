b45b7b95ffaa8a30b35a17f63d9ddd334ff25a60
Hamit Tırpan <hamittirpan@gmail.com>
2019-10-16 14:53:16 +0300

    Initial Commit

---
+++ MLImageRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLImageRecognition
+//
+//  Created by Hamit  Tırpan on 16.10.2019.
+//  Copyright © 2019 Hamit  Tırpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
